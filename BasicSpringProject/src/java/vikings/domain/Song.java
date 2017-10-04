75d7570ac4744e9a19b7b4dbd471d90ba4660bd6
Bryan Koelbel <bryankoelbel@Bryans-Little-Mac.local>
2017-10-04 18:11:15 -0400

    Added sample Spring project

---
+++ BasicSpringProject/src/java/vikings/domain/Song.java
@@ -0,0 +1,34 @@
+package vikings.domain;
+
+/**
+ * This is an example POJO for a Song (obviously simplified)
+ */
+public class Song {
+    private String id;
+    private String name;
+    private String artist;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+}
---
+++ BasicSpringProject/web/WEB-INF/jsp/index.jsp
@@ -0,0 +1,15 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+    "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Sample Spring Project</title>
+    </head>
+
+    <body>
+        <h2>Hello! Here are two Songs.</h2>
+        <p> <a href="song/123.htm">See Song with ID 123</a> </p>
+        <p> <a href="song/456.htm">See Song with ID 456</a> </p>
+    </body>
+</html>
