4fecce19d871761c15fe4507cb16e0c4ef83d1a2
Bryan Koelbel <bryankoelbel@Bryans-Little-Mac.local>
2017-10-27 13:14:52 -0400

    Added SongDAO method stub

---
+++ 308Vikings/src/main/java/com/vikings/dao/SongDAO.java
@@ -0,0 +1,18 @@
+package com.vikings.dao;
+
+import com.vikings.domain.Song;
+import org.springframework.stereotype.Repository;
+
+/**
+ * DAO for basic Song actions
+ */
+@Repository
+public class SongDAO {
+    
+    //@TODO
+    public Song getSong(String id) {
+        //@TODO
+        return null;
+    }
+    
+}
