0cef12eee443a64575eee085fbc6f6eb75567141
sunwawu <sunwa.wu.7896@gmail.com>
2017-11-12 22:07:47 -0500

    Added View Artist Page

---
+++ 308Vikings/src/main/java/com/vikings/domain/requests/ArtistPageResponse.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.vikings.domain.requests;
+
+import com.vikings.domain.Song;
+import com.vikings.domain.identifier.AlbumIdentifier;
+import com.vikings.domain.identifier.ArtistIdentifier;
+import java.util.List;
+
+public class ArtistPageResponse {
+ List<Song> topSongs;
+ List<ArtistIdentifier> relatedArtists;
+ List<AlbumIdentifier> albums;
+ 
+ public ArtistPageResponse(){
+ }
+ 
+ public ArtistPageResponse(List<Song> topSongs, List<ArtistIdentifier> 
+         relatedArtists,List<AlbumIdentifier> albums){
+  this.topSongs = topSongs;
+  this.relatedArtists = relatedArtists;
+  this.albums = albums;
+ }
+ 
+ public List<Song> getTopSongs(){
+  return topSongs;
+ }
+ 
+ public void setTopSongs(List<Song> topSongs){
+  this.topSongs = topSongs;
+ }
+ 
+ public List<ArtistIdentifier> getRelatedArtists(){
+  return relatedArtists;
+ }
+ 
+ public void setRelatedArtists(List<ArtistIdentifier> relatedArtists){
+  this.relatedArtists = relatedArtists;
+ }
+ 
+ public List<AlbumIdentifier> getAlbums(){
+  return albums;
+ }
+ 
+ public void setAlbums(List<AlbumIdentifier> albums){
+  this.albums = albums;
+ }
+}
